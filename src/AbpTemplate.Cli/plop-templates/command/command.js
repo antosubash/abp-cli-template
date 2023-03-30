5ff91f9733a0f14fcdfc6a4a193744769de67480
Anto Subash <antosubash@live.com>
2023-03-30 14:37:17 +0200

    added api parameter and move plop to the root

--- src/AbpTemplate.Cli/plop-templates/command/command.js
+++ src/AbpTemplate.Cli/plop-templates/command/command.js
@@ -17,39 +17,39 @@ module.exports = {
         if (data.folder) {
             actions.push({
                 type: "add",
-                path: "AbpTemplate.Cli/Commands/{{pascalCase folder}}/{{pascalCase name}}/{{pascalCase name}}Command.cs",
+                path: "Commands/{{pascalCase folder}}/{{pascalCase name}}/{{pascalCase name}}Command.cs",
                 templateFile: "plop-templates/command/command.cs.hbs",
             });
         } else {
             actions.push({
                 type: "add",
-                path: "AbpTemplate.Cli/Commands/{{pascalCase name}}/{{pascalCase name}}Command.cs",
+                path: "Commands/{{pascalCase name}}/{{pascalCase name}}Command.cs",
                 templateFile: "plop-templates/command/command.cs.hbs",
             });
         }
 
         actions.push({
             type: "add",
-            path: "AbpTemplate.Cli/Services/{{pascalCase name}}Service.cs",
+            path: "Services/{{pascalCase name}}Service.cs",
             templateFile: "plop-templates/services/service.cs.hbs",
         })
         actions.push({
             type: "append",
-            path: "AbpTemplate.Cli/Program.cs",
+            path: "Program.cs",
             pattern: "// PLOP_SERVICE_REGISTRATION",
             template: "        registrations.AddScoped<{{pascalCase name}}Service>();",
         });
 
         actions.push({
             type: "append",
-            path: "AbpTemplate.Cli/Program.cs",
+            path: "Program.cs",
             pattern: "// PLOP_COMMAND_REGISTRATION",
             template: '            config.AddCommand<{{pascalCase name}}Command>("{{name}}");',
         })
 
         actions.push({
             type: "append",
-            path: "AbpTemplate.Cli/Program.cs",
+            path: "Program.cs",
             pattern: "// PLOP_INJECT_USING",
             template: 'using AbpTemplate.Cli.Commands.{{pascalCase name}};',
         })
